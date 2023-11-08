cd8d9e38e24203c330d9f6a5c0f976d49bea0f1b
Stefano <snebo54@hotmail.com>
2023-11-09 00:24:19 +0100

    Fixed:     Scaling issues when using differnt grid sizes     grid incorrectly refreshing with gaps

--- scripts/script.js
+++ scripts/script.js
@@ -3,33 +3,35 @@ let change_grid  = document.querySelector(".change_grid")
 
 let grid_input = 16
 let count
-const grid = grid_input*grid_input
+let grid
 
 function gridSetup(){
     container.innerHTML=""
+    grid = grid_input*grid_input
     count= 0
 
     while(count < grid){
         newDiv = document.createElement('div')
         newDiv.classList.add("box")
-        newDiv.style.cssText =`flex:1 0 ${(98/(grid/grid_input))}%; height:${100/(grid/grid_input)}%; border:1px solid grey`
+        newDiv.style.cssText =`flex:1 0 ${(100/(grid/grid_input))}%; height:${100/(grid/grid_input)}%; border:1px solid grey`
         container.appendChild(newDiv)
         count++
     }
+    pixelShader()
 }
-gridSetup()
+function pixelShader(){
+    const pixels = document.querySelectorAll('.box')
+    pixels.forEach(pixel => pixel.addEventListener("mouseover", ()=>{
+        pixel.style.cssText += "background-color: black"
+    }))
+}
 
 change_grid.addEventListener("click",()=>{
     let catcher = Number(prompt("Enter a grid size (eg 32, 16,...)"))
-    console.log(typeof catcher)
     if (typeof catcher == "number"){
         grid_input = catcher
         gridSetup()
     } 
 })
 
-console.log(count)
-const pixels = document.querySelectorAll('.box')
-pixels.forEach(pixel => pixel.addEventListener("mouseover", ()=>{
-    pixel.style.cssText += "background-color: black"
-}))
+gridSetup()
