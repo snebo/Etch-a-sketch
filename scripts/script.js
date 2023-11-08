26e9c58920d0f07fab11f349bb79dd65ab23076c
Stefano <snebo54@hotmail.com>
2023-11-09 00:06:10 +0100

    Added refresh grid functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,33 @@
-container = document.querySelector(".container")
+let container = document.querySelector(".container")
+let change_grid  = document.querySelector(".change_grid")
 
-const grid_input = 16
-let count = 0
+let grid_input = 16
+let count
 const grid = grid_input*grid_input
 
-//initilize grid
-while(count < grid){
-    newDiv = document.createElement('div')
-    newDiv.classList.add("box")
-    newDiv.style.cssText =`flex:1 0 ${(98/(grid/grid_input))}%; height:${100/(grid/grid_input)}%; border:1px solid grey`
-    container.appendChild(newDiv)
-    count++
+function gridSetup(){
+    container.innerHTML=""
+    count= 0
+
+    while(count < grid){
+        newDiv = document.createElement('div')
+        newDiv.classList.add("box")
+        newDiv.style.cssText =`flex:1 0 ${(98/(grid/grid_input))}%; height:${100/(grid/grid_input)}%; border:1px solid grey`
+        container.appendChild(newDiv)
+        count++
+    }
 }
+gridSetup()
+
+change_grid.addEventListener("click",()=>{
+    let catcher = Number(prompt("Enter a grid size (eg 32, 16,...)"))
+    console.log(typeof catcher)
+    if (typeof catcher == "number"){
+        grid_input = catcher
+        gridSetup()
+    } 
+})
+
 console.log(count)
 const pixels = document.querySelectorAll('.box')
 pixels.forEach(pixel => pixel.addEventListener("mouseover", ()=>{
