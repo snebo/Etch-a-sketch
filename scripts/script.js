690fd8559d22490a9e0e1fceaaa30ad18477a448
Stefano <snebo54@hotmail.com>
2023-11-08 21:21:48 +0100

    Added hover effect to mouse pointer

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ let count = 0
 while(count < (rows*cols)){
     const newDiv = document.createElement('div')
     newDiv.classList.add("box")
-    // newDiv.style.cssText ="flex:1 0 6.25%; height:100%; border:1px solid black;"
+    // newDiv.style.cssText ="flex:1 0 6.25%; height:100%; border:1px solid black;" // use this to calculate grid size change
     container.appendChild(newDiv)
     count++
 }
