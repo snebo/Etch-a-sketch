6d63ab86cd73c563b63701ba2b08c035a7fcdb91
Stefano <snebo54@hotmail.com>
2023-11-08 23:05:54 +0100

    Moved box styles to js and added hover paint

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,15 @@ const rows= 16
 const cols= 16
 let count = 0
 
+//initilize grid
 while(count < (rows*cols)){
-    const newDiv = document.createElement('div')
+    newDiv = document.createElement('div')
     newDiv.classList.add("box")
-    // newDiv.style.cssText ="flex:1 0 6.25%; height:100%; border:1px solid black;" // use this to calculate grid size change
+    newDiv.style.cssText ="flex:1 0 6%; height:6.25%; border:1px solid grey" 
     container.appendChild(newDiv)
     count++
 }
+const pixels = document.querySelectorAll('.box')
+pixels.forEach(pixel => pixel.addEventListener("mouseover", ()=>{
+    pixel.style.cssText += "background-color: black"
+}))
