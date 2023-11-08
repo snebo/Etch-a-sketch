8ce3f973e20f353d3f3d1130aa198fcabb4e045d
Stefano <snebo54@hotmail.com>
2023-11-08 23:37:51 +0100

    Fix: styles and js logic; Added button to change grid size

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,18 @@
 container = document.querySelector(".container")
 
-const rows= 16
-const cols= 16
+const grid_input = 16
 let count = 0
+const grid = grid_input*grid_input
 
 //initilize grid
-while(count < (rows*cols)){
+while(count < grid){
     newDiv = document.createElement('div')
     newDiv.classList.add("box")
-    newDiv.style.cssText ="flex:1 0 6%; height:6.25%; border:1px solid grey" 
+    newDiv.style.cssText =`flex:1 0 ${(98/(grid/grid_input))}%; height:${100/(grid/grid_input)}%; border:1px solid grey`
     container.appendChild(newDiv)
     count++
 }
+console.log(count)
 const pixels = document.querySelectorAll('.box')
 pixels.forEach(pixel => pixel.addEventListener("mouseover", ()=>{
     pixel.style.cssText += "background-color: black"
